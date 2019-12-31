0bed1827a663578d44a85aabb0ed2b532d1679be
maxiwebs <maxiwebs@gmail.com>
2019-12-31 17:26:18 -0300

    Mejoro la version anterior. En esta no hay subsecuencias.

--- corsi/research/theory.py
+++ corsi/research/theory.py
@@ -2,19 +2,41 @@ import trials_raw
 import itertools
 import csv
 
+#Chequea que el trial no sea subsecuencia de los ya cargados
 def notIsSubsequence(trial,selected_trials):
-	notIsSubsequence = True
 	trial = "".join(trial)
 	print selected_trials	
-	#Veo si no hay substrings con los anteriores
+	#Veo si no hay substrings con los anteriores (los de mayor longitud)
 	for i in range(len(trial),len(selected_trials)):
 		print "Chequeo si "+trial+" es substring de: "+selected_trials[i][0]
 		if trial in str(selected_trials[i][0]):
 			print(trial, " is in ",selected_trials[i][0])
 			return False
 				
+	return True
+
+#Chequea que el trial no contenga subsecuencias (tomando de a 2) de los ya cargados
+def notHasSubsequence(trial,selected_trials):
+	print ("Analizo: "+"".join(trial))
+	#Veo si no hay substrings con los anteriores (los de mayor longitud)
+	for j in range(len(trial),len(selected_trials)):
+		#Si no hay con quien analizar, salto
+		if selected_trials[j][0] == 'X':
+			return True
+
+		#Tomo el trial de a 2
+		for i in range(1,len(trial)):
+			deAdos = []
+			deAdos.append(trial[i-1])
+			deAdos.append(trial[i])
+			junto = "".join(deAdos)
+			print "Chequeo si "+junto+" es substring de: "+selected_trials[j][0]
+			if junto in str(selected_trials[j][0]):
+				print(junto, " is in ",selected_trials[j][0])
+				return False
 
 	return True
+
 
 #Cuenta la cantidad de apariciones de cada letra en los trials seleccionados
 def cantApariciones(letters,selected_trials):
@@ -48,7 +70,7 @@ for i in range(8,0,-1):
 						trials_raw.frontness(trial),    # frontness length
 						sum(trials_raw.distances(trial)),    # length
 			]) #ntrial,  umber, leftness, frontness, length
-			if sum(trials_raw.distances(trial)) >= max_distance and notIsSubsequence(trial,selected_trials):
+			if sum(trials_raw.distances(trial)) >= max_distance and notHasSubsequence(trial,selected_trials):
 				print("Va ganando ","".join(trial)," con distancia: ",sum(trials_raw.distances(trial)))
 				max_distance = sum(trials_raw.distances(trial))
 				data_trial = ["".join(trial),sum(trials_raw.distances(trial))]
