7c58a4c60d497bb6b3f9badc6a54ac68a8dc67dd
maxiwebs <maxiwebs@gmail.com>
2019-12-31 15:59:24 -0300

    Ok, genera trials sin substrings con distancia maxima.

--- corsi/research/theory.py
+++ corsi/research/theory.py
@@ -4,18 +4,25 @@ import csv
 
 def notIsSubsequence(trial,selected_trials):
 	notIsSubsequence = True
+	trial = "".join(trial)
 	print selected_trials	
 	#Veo si no hay substrings con los anteriores
-	for i in range(0,len(trial)-1):
-		print "Chequeo si "+"".join(trial)+" tiene algun elemento en: "+selected_trials[i][0]
-		for j in range(0,len(trial)):
-			if str(trial[j]) in str(selected_trials[i][0]):
-				print(str(trial[j]), " is in ",selected_trials[i][0])
-				return False
+	for i in range(len(trial),len(selected_trials)):
+		print "Chequeo si "+trial+" es substring de: "+selected_trials[i][0]
+		if trial in str(selected_trials[i][0]):
+			print(trial, " is in ",selected_trials[i][0])
+			return False
 				
 
 	return True
 
+#Cuenta la cantidad de apariciones de cada letra en los trials seleccionados
+def cantApariciones(letters,selected_trials):
+	for letter in letters:
+		apariciones = (str(selected_trials)).count(letter)
+		print letter+" aparece: "+str(apariciones)
+
+	return cantApariciones
 
 print "###################################################################################"
 letters = sorted(trials_raw.box_positions.keys())
@@ -26,9 +33,9 @@ data = []
 seqs = []
 data.append(["Trial","NumberMoves","Leftness","Frontness","Length"])
 max_distance = 0
-selected_trials = [['X',0],['X',0],['X',0],['X',0],['X',0]]
+selected_trials = [['X',0],['X',0],['X',0],['X',0],['X',0],['X',0],['X',0],['X',0]]
 data_trial = []
-for i in range(1,6):
+for i in range(8,0,-1):
 	max_distance = 0
 	c = 0
 	for seq in itertools.combinations(letters,i):
@@ -41,13 +48,14 @@ for i in range(1,6):
 						trials_raw.frontness(trial),    # frontness length
 						sum(trials_raw.distances(trial)),    # length
 			]) #ntrial,  umber, leftness, frontness, length
-			if sum(trials_raw.distances(trial)) >= max_distance: # and notIsSubsequence(trial,selected_trials):
+			if sum(trials_raw.distances(trial)) >= max_distance and notIsSubsequence(trial,selected_trials):
 				print("Va ganando ","".join(trial)," con distancia: ",sum(trials_raw.distances(trial)))
 				max_distance = sum(trials_raw.distances(trial))
 				data_trial = ["".join(trial),sum(trials_raw.distances(trial))]
 				selected_trials[i-1] = data_trial
 	#print("For ", i, " there are ", c, "different trials")
 print selected_trials
+cantApariciones(letters,selected_trials)
 
 with open("theory.csv", 'w') as fp:
     a = csv.writer(fp, delimiter=';')
