36ae2853acc76bf32d071d859b69bda7c7a41f06
maxiwebs <maxiwebs@gmail.com>
2019-12-31 11:29:56 -0300

    Reparado theory.py porque grababa duplicados.

--- corsi/research/theory.py
+++ corsi/research/theory.py
@@ -1,5 +1,3 @@
-
-
 import trials_raw
 import itertools
 import csv
@@ -10,19 +8,21 @@ letters = sorted(trials_raw.box_positions.keys())
 source = "".join(letters)
 
 data = []
+seqs = []
 data.append(["Trial","NumberMoves","Leftness","Frontness","Length"])
-for i in range(1, 9+1):
+for i in range(1, 8+1):
     c = 0
     for seq in itertools.combinations(letters,i):
-        # print ("for ", "".join(seq))
+        #print ("for ", "".join(seq))
         for trial in itertools.permutations(seq):
-            c += 1
-            data.append(["".join(seq),                  #trial
-                        i,                             # number
-                        trials_raw.leftness(trial),     # leftness
-                        trials_raw.frontness(trial),    # frontness length
-                        sum(trials_raw.distances(trial)),    # length
-                    ]) #ntrial,  umber, leftness, frontness, length
+			c += 1
+			data.append(["".join(trial),                  #trial
+						i,                             # number
+						trials_raw.leftness(trial),     # leftness
+						trials_raw.frontness(trial),    # frontness length
+						sum(trials_raw.distances(trial)),    # length
+			]) #ntrial,  umber, leftness, frontness, length
+
 
     print ("For ", i, " there are ", c, "different trials")
 
